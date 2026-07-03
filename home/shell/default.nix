d99b938f8480bbf190eac36d8d65503a8fd85347
wwhasmile <uwuhasmile@proton.me>
2026-07-03 19:10:08 +0300

    Shell module

---
+++ home/shell/default.nix
@@ -0,0 +1,69 @@
+{ config, lib, pkgs, ... }:
+
+let
+  cfg = config.homeSettings.shell;
+in {
+  options = {
+    homeSettings.shell = {
+      enable = lib.mkEnableOption "Enable fancy shell";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    programs.zsh = {
+      enable = true;
+      syntaxHighlighting.enable = true;
+      enableCompletion = true;
+      autosuggestion.enable = true;
+
+      shellAliases = {
+        neofetch = "disfetch";
+      };
+
+      initContent = ''
+        disfetch
+      '';
+
+      plugins = [
+        {
+          name = "zsh-nix-shell";
+          file = "nix-shell.plugin.zsh";
+          src = pkgs.fetchFromGitHub {
+            owner = "chisui";
+            repo = "zsh-nix-shell";
+            rev = "v0.8.0";
+            sha256 = "1lzrn0n4fxfcgg65v0qhnj7wnybybqzs4adz7xsrkgmcsr0ii8b7";
+          };
+        }
+        {
+          name = "spaceship-prompt";
+          file = "spaceship.zsh";
+          src = pkgs.fetchFromGitHub {
+            owner = "spaceship-prompt";
+            repo = "spaceship-prompt";
+            rev = "v4.22.5";
+            sha256 = "10c905cv2dybcqg2xigv60z71anaf1r6vlnfh83idhcjyc56048r";
+          };
+        }
+      ];
+    };
+
+    programs.bash = {
+      enable = true;
+      enableCompletion = true;
+      shellAliases = config.programs.zsh.shellAliases;
+    };
+
+    programs.neovim = {
+      enable = true;
+      viAlias = true;
+      vimAlias = true;
+    };
+
+    home.packages = with pkgs; [
+      disfetch
+      gnugrep
+      gnused
+    ];
+  };
+}
--- hosts/narcissus/home.nix
+++ hosts/narcissus/home.nix
@@ -5,15 +5,13 @@
     name = "wwhasmile";
     email = "uwuhasmile@proton.me";
 
+    shell.enable = true;
+
     browser.enable = true;
     chat.enable = true;
 
     git.enable = true;
   };
-
-  home.packages = with pkgs; [
-    fastfetch
-  ];
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
