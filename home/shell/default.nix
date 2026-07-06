20473d2e2d8936705b74a3433d88ecc099242932
wwhasmile <uwuhasmile@proton.me>
2026-07-06 23:21:34 +0300

    Direnv added to shell

--- home/shell/default.nix
+++ home/shell/default.nix
@@ -14,50 +14,54 @@ in {
   };
 
   config = lib.mkIf cfg.enable {
-    programs.zsh = {
-      enable = true;
-      syntaxHighlighting.enable = true;
-      enableCompletion = true;
-      autosuggestion.enable = true;
+    programs = {
+      zsh = {
+        enable = true;
+        syntaxHighlighting.enable = true;
+        enableCompletion = true;
+        autosuggestion.enable = true;
 
-      shellAliases = {
-        neofetch = "fastfetch";
+        shellAliases = {
+          neofetch = "fastfetch";
+        };
+
+        plugins = [
+          {
+            name = "zsh-nix-shell";
+            file = "nix-shell.plugin.zsh";
+            src = pkgs.fetchFromGitHub {
+              owner = "chisui";
+              repo = "zsh-nix-shell";
+              rev = "v0.8.0";
+              sha256 = "1lzrn0n4fxfcgg65v0qhnj7wnybybqzs4adz7xsrkgmcsr0ii8b7";
+            };
+          }
+          {
+            name = "spaceship-prompt";
+            file = "spaceship.zsh";
+            src = pkgs.fetchFromGitHub {
+              owner = "spaceship-prompt";
+              repo = "spaceship-prompt";
+              rev = "v4.22.5";
+              sha256 = "10c905cv2dybcqg2xigv60z71anaf1r6vlnfh83idhcjyc56048r";
+            };
+          }
+        ];
       };
 
-      plugins = [
-        {
-          name = "zsh-nix-shell";
-          file = "nix-shell.plugin.zsh";
-          src = pkgs.fetchFromGitHub {
-            owner = "chisui";
-            repo = "zsh-nix-shell";
-            rev = "v0.8.0";
-            sha256 = "1lzrn0n4fxfcgg65v0qhnj7wnybybqzs4adz7xsrkgmcsr0ii8b7";
-          };
-        }
-        {
-          name = "spaceship-prompt";
-          file = "spaceship.zsh";
-          src = pkgs.fetchFromGitHub {
-            owner = "spaceship-prompt";
-            repo = "spaceship-prompt";
-            rev = "v4.22.5";
-            sha256 = "10c905cv2dybcqg2xigv60z71anaf1r6vlnfh83idhcjyc56048r";
-          };
-        }
-      ];
-    };
+      bash = {
+        enable = true;
+        enableCompletion = true;
+        shellAliases = config.programs.zsh.shellAliases;
+      };
 
-    programs.bash = {
-      enable = true;
-      enableCompletion = true;
-      shellAliases = config.programs.zsh.shellAliases;
-    };
+      neovim = {
+        enable = true;
+        viAlias = true;
+        vimAlias = true;
+      };
 
-    programs.neovim = {
-      enable = true;
-      viAlias = true;
-      vimAlias = true;
+      direnv.enable = true;
     };
 
     home.packages = with pkgs; [
