82983d2971357eeac6d38f7c1233dfc7fe5a0bd6
wwhasmile <uwuhasmile@proton.me>
2026-07-08 20:52:32 +0300

    Making file search recursive in home modules

--- home/default.nix
+++ home/default.nix
@@ -1,14 +1,6 @@
 { lib, ... }:
 
-with lib;
-let
-  imports = builtins.readDir ./. |>
-    lib.filterAttrs (name: value:
-      (value == "directory" && builtins.pathExists ./${name}/default.nix) ||
-      (lib.hasSuffix ".nix" name && name != "default.nix")
-    ) |>
-    builtins.attrNames |>
-    map (x: (toString ./.) + "/${x}");
-in {
-    inherit imports;
-  }
+{
+  imports = lib.filesystem.listFilesRecursive ./. |>
+    lib.filter (file: lib.strings.hasSuffix ".nix" file && file != ./default.nix);
+}
