9e8367f0dbc2d760c5d65813989634ae4c57f3ca
wwhasmile <uwuhasmile@proton.me>
2026-07-09 17:39:55 +0300

    Replaced port params in firewall with Syncthing port

--- system/security/firewalld.nix
+++ system/security/firewalld.nix
@@ -6,22 +6,13 @@ in {
   options = {
     systemSettings.firewalld = {
       enable = lib.mkEnableOption "Enable firewall";
-      tcpPorts = lib.mkOption {
-        type = lib.types.listOf lib.types.int;
-        description = "TCP ports to open";
-      };
-    udpPorts = lib.mkOption {
-        type = lib.types.listOf lib.types.int;
-        description = "UDP ports to open";
-      };
     };
   };
 
   config = lib.mkIf cfg.enable {
     networking.firewall = {
       enable = true;
-      allowedTCPPorts = cfg.tcpPorts;
-      allowedUDPPorts = cfg.udpPorts;
+      allowedTCPPorts = [ 8384 ]; # Syncthing
     };
   };
 }
