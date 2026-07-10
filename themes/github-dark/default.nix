c95681ab863db846cb1600b1b7f59a63cc148bed
wwhasmile <uwuhasmile@proton.me>
2026-07-10 12:38:55 +0300

    Fixing Github Dark theme

--- themes/github-dark/default.nix
+++ themes/github-dark/default.nix
@@ -1,7 +1,24 @@
 pkgs:
 
 {
-  scheme = "${pkgs.base16-schemes}/share/themes/github-dark.yaml";
+  scheme = {
+    base00 = "#0d1117";
+    base01 = "#161b22";
+    base02 = "#484f58";
+    base03 = "#6e7681";
+    base04 = "#8b949e";
+    base05 = "#c9d1d9";
+    base06 = "#f0f6fc";
+    base07 = "#ffffff";
+    base08 = "#ffa657";
+    base09 = "#79c0ff";
+    base0A = "#bb8009";
+    base0B = "#a5d6ff";
+    base0C = "#7ee787";
+    base0D = "#d2a8ff";
+    base0E = "#ff7b72";
+    base0F = "#ffa198";
+  };
   polarity = "dark";
 
   background = {
